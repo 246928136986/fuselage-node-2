cbb48597e5b151bc07dc86665afdd05b430603d0
246928136986 <math.trigonometry.alt.291@gmail.com>
2025-03-08 21:15:04 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
     prefix: '/embed/',
     
     // BARE SERVER
-    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    bare:'https://nauka.politechnika-nova.edu.pl/',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
